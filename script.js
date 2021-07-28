bf66c8bf48db7d7aea209cf2d380548268812830
therealrajan <therealrajan@gmail.com>
2021-07-28 18:15:08 +0545

    Added local files to remote repo

---
+++ script.js
@@ -0,0 +1,27 @@
+const sounds = ['Applause', 'Boo', 'Yay', 'Tada' ,'Victory', 'Wrong', 'Alarm', 'Woof','Laugh','Sneeze', 'Orchestra', 'Trumpets','Yawn', 'Crunch', 'Mystery', 'WarHorn']
+
+
+sounds.forEach(sound=>{
+    const btn = document.createElement("button")
+    btn.classList.add("btn")
+    btn.innerText = sound
+
+    btn.addEventListener('click', ()=>{
+        
+        const soundClass = "." + sound //To use querySelector to select elememts with class-name
+        stopSongs()
+        document.querySelector(soundClass).play()
+    })
+
+    document.querySelector('.buttons')
+    .appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound =>{
+        const soundClass = "." + sound
+        const song = document.querySelector(soundClass)
+        song.pause()
+        song.currentTime = 0
+    })
+}
