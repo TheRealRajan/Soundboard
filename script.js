ed1cce68f1b18458b0af47a12a13f5597ec6017f
therealrajan <therealrajan@gmail.com>
2021-07-28 18:58:43 +0545

    Added more sound effects and changed the styling of the buttons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sounds = ['Applause', 'Boo', 'Yay', 'Tada' ,'Victory', 'Wrong', 'Alarm', 'Woof','Laugh','Sneeze', 'Orchestra', 'Trumpets','Yawn', 'Crunch', 'Mystery', 'WarHorn']
+const sounds = ['Applause', 'Boo', 'Yay', 'Tada' ,'Victory', 'Wrong', 'Alarm','BuDumTss', 'Woof','Laugh','TskTsk','Sneeze', 'Orchestra', 'Trumpets','Yawn', 'Crunch', 'Mystery', 'WarHorn', 'Thunder', 'Horror']
 
 
 sounds.forEach(sound=>{
